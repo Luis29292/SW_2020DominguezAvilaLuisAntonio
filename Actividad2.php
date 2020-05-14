a5b68b3ae96c0cb4f4ae483ea312550205333754
Luis29292 <luis_dominguez@comunidad.unam.mx>
2020-05-14 13:04:11 -0500

    FEAT: Creé el archivo Actividad2.php con Mi nombre y 2 cosas que me han gustdo del curso

---
+++ Actividad2.php
@@ -0,0 +1,9 @@
+<?php
+  echo
+  "
+    Domínguez Ávila Luis Antonio <br>
+    <ul>Aprendí y me gustó
+      <li>El uso de Karel y la elaboración de diagramas
+      <li>Para qué sirve Github
+  "
+ ?>
