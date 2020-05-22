244070e02cbcc2701a0dfb476d4be8cd2f620ffa
Luis29292 <luis_dominguez@comunidad.unam.mx>
2020-05-22 18:12:01 -0500

    FEAT: Se creó la carpeta para la actividad2.php

---
+++ Actividad2/Actividad2.php
@@ -0,0 +1,10 @@
+<?php
+  echo
+  "
+    Domínguez Ávila Luis Antonio <br>
+    <ul>Aprendí y me gustó
+      <li>El uso de Karel y la elaboración de diagramasz</li>
+      <li>Para qué sirve Github
+    </ul>
+  ";
+ ?>
