52768b15173e1d5b80e2b3e0f03fed975e1d559c
Luis29292 <luis_dominguez@comunidad.unam.mx>
2020-05-21 23:13:46 -0500

    FIX: Se corre la descripción de la actividad

--- Actividad2.php
+++ Actividad2.php
@@ -3,7 +3,7 @@
   "
     Domínguez Ávila Luis Antonio <br>
     <ul>Aprendí y me gustó
-      <li>El uso de Karel y la elaboración de diagramas
+      <li>El uso de Karel y la elaboración de diagramasz</li>
       <li>Para qué sirve Github
     </ul>
   ";
