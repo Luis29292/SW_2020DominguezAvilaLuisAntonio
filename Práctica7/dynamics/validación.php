139a7bfc0714378ec953e7f53177235b0683fb39
Luis29292 <luis_dominguez@comunidad.unam.mx>
2020-05-23 16:43:25 -0500

    FEAT: Se incluyó la validación de una contraseña segura (7.2)

--- Práctica7/dynamics/validación.php
+++ Práctica7/dynamics/validación.php
@@ -7,9 +7,14 @@
     $_POST['nac']=> '/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/',
     $rfc=$_POST['RFC'] => '/[A-Z]{4}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])([0-9]|[A-Z]){3}/'
   ];
+  $pas=$_POST['contra'];
   foreach ($val as $key => $value) {
     if (!preg_match($value,$key)){
       echo "<<".$key.">> no es un dato válido <br>";
     }
+  }
+  if (!preg_match('/(?=.*[A-Z])(?=.*[\.,!@#$&*])(?=.*[0-9])(?=.*[a-z]).{10,}/',$pas))
+  {
+    echo "Su contrasena: ".$pas." no es segura";
   }
 ?>
